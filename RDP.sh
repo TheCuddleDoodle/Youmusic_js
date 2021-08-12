db7c713098245d0aee59293deda6e2127d583791
TheCuddleDoodle <50615549+TheCuddleDoodle@users.noreply.github.com>
2021-08-12 18:53:22 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m curb
 sudo adduser curb sudo
-echo 'new:new' | sudo chpasswd
+echo 'curb:curb' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
